ab19c1574353031c7c4de5209f9424e310cf36ad
白瑀琦 <1339743454@qq.com>
2022-12-08 08:55:34 +0800

    建造者模式

---
+++ code/Builder/Builder.java
@@ -0,0 +1,10 @@
+/**
+ * @author Yuukin
+ * @data 2022/12/8 8:22
+ */
+public abstract class Builder {
+    public abstract void makeTitle(String title);
+    public abstract void makeString(String str);
+    public abstract void makeItems(String[] items);
+    public abstract void close();
+}
